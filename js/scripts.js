fb63b759fe18cb941f1942c0f5c3925187d2c159
Jared Gain <jlgain7@gmail.com>
2020-03-21 14:50:26 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,44 +10,48 @@ const galleryDiv = document.querySelector('.gallery');
  */ 
 
 // Fetch Data and return result
-function fetchData (url)
-{
-    return fetch(url)
-        .then(checkStatus)
-        .then(res => res.json())
-        .catch(error => console.log('There was a problem with the request.', error));
-    }
+fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then(data => console.log(data));
 
-Promise.all(
-    [
-        fetchData('')
-    ]    
-)
-.then(data => 
-    {
+// function fetchData (url)
+// {
+//     return fetch(url)
+//         .then(checkStatus)
+//         .then(res => res.json())
+//         .catch(error => console.log('There was a problem with the request.', error));
+//     }
 
-    }
-)
+// Promise.all(
+//     [
+//         fetchData('https://randomuser.me/api/')
+//     ]    
+// )
+// .then(data => 
+//     {
+
+//     }
+// )
 
 /**
  * Helper Functions
  */
 
 // Check Status of Response
-function checkStatus (response)
-{
-    if(response.ok)
-    {
-        return Promise.resolve(response);
-    }
-    else
-    {
-        return Promise.reject(new Error(resposne.statusText));
-    }
-}
+// function checkStatus (response)
+// {
+//     if(response.ok)
+//     {
+//         return Promise.resolve(response);
+//     }
+//     else
+//     {
+//         return Promise.reject(new Error(resposne.statusText));
+//     }
+// }
 
-// Generate Photos 
-function (generatePhoto(data))
-{
+// // Generate Photos 
+// function (generatePhoto(data))
+// {
 
-}
+// }
